fd78d66175a3652fee6a697adb58aa3a90ce0fcb
leandroltfx <leandroltfbr@hotmail.com>
2025-07-07 08:16:34 -0300

    iniciando projeto

---
+++ src/main/java/br/com/leandroltfx/finance_control_back_spring_core/FinanceControlBackSpringCoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.leandroltfx.finance_control_back_spring_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceControlBackSpringCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceControlBackSpringCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leandroltfx/finance_control_back_spring_core/FinanceControlBackSpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandroltfx.finance_control_back_spring_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlBackSpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
