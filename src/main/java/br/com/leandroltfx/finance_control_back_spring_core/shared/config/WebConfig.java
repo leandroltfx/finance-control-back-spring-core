364328281c9e379137e75e74cb4e36028ec796f8
leandroltfx <leandroltfbr@hotmail.com>
2025-07-10 21:53:17 -0300

    feat(config): configurando CORS

---
+++ src/main/java/br/com/leandroltfx/finance_control_back_spring_core/shared/config/WebConfig.java
@@ -0,0 +1,25 @@
+package br.com.leandroltfx.finance_control_back_spring_core.shared.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+                        // .allowCredentials(true); // cookies/autenticação
+            }
+        };
+    }
+}
